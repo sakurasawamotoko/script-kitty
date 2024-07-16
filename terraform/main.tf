7799c16bd8aa1ab5c68e03b26ac9df8c4b896b91
Motoko Sakurasawa <111317751+sakurasawamotoko@users.noreply.github.com>
2024-07-16 08:43:38 -0700

    初期化

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_lambda_function" "example" {
+  function_name = "example_lambda"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "main.lambda_handler"
+  runtime       = "python3.9"
+  filename      = data.archive_file.lambda_zip.output_path
+}
+
+resource "aws_iam_role" "iam_for_lambda" {
+  name = "iam_for_lambda"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+    }]
+  })
+}
+
+data "archive_file" "lambda_zip" {
+  type        = "zip"
+  source_dir  = "${path.module}/src"
+  output_path = "${path.module}/lambda.zip"
+}
