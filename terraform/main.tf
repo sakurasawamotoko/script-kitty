c538809d85bb3efd6147d652357710a9a16147a9
Motoko Sakurasawa <111317751+sakurasawamotoko@users.noreply.github.com>
2024-08-23 21:55:00 -0700

    path.module が変更された

--- terraform/main.tf
+++ terraform/main.tf
@@ -27,6 +27,6 @@ resource "aws_iam_role" "iam_for_lambda" {
 
 data "archive_file" "lambda_zip" {
   type        = "zip"
-  source_dir  = "${path.module}/src"
-  output_path = "${path.module}/lambda.zip"
+  source_dir  = "../src"
+  output_path = "../lambda.zip"
 }
