d294d0edbb1906172191ea58cdbf6457a8d6616b
Motoko Sakurasawa <111317751+sakurasawamotoko@users.noreply.github.com>
2024-08-23 23:01:34 -0700

    name -> name_prefix

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,7 +11,7 @@ resource "aws_lambda_function" "example" {
 }
 
 resource "aws_iam_role" "iam_for_lambda" {
-  name = "iam_for_lambda"
+  name_prefix = "iam_for_lambda"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
