2543b53dc8236a4354c4870c8b9b84166a0b6236
Motoko Sakurasawa <111317751+sakurasawamotoko@users.noreply.github.com>
2024-08-25 11:17:25 -0700

    ロール機能性

--- src/main.py
+++ src/main.py
@@ -3,12 +3,13 @@ import os
 import discord
 from discord.ext import tasks, commands
 from dotenv import load_dotenv
-
-intents = discord.Intents.default()
-intents.members = True  # Enable member intents
+from time import sleep
 
 # Load env vars from .env
 load_dotenv()
+
+intents = discord.Intents.all()
+#intents.members = True  # Enable member intents
 
 # Initialize bot with command prefix and intents
 bot = commands.Bot(command_prefix='!', intents=intents)
@@ -20,12 +21,12 @@ ROLE_TWITCHSUB_ID = int(os.environ['DISCORD_ROLE_DISCORD_BOOSTER_ID'])
 ROLE_BOOSTER_ID = int(os.environ['DISCORD_ROLE_TWITCH_SUB_ID'])
 ROLE_SUPPORTER_ID = int(os.environ['DISCORD_ROLE_SUPPORTERS_ID'])
 
-@tasks.loop(minutes=10)  # Check every 10 minutes
-async def check_roles():
+@bot.event
+async def on_ready():
     guild = bot.get_guild(GUILD_ID)
+
     if guild is None:
         print("Guild not found")
-        return
 
     role_twitchsub = guild.get_role(ROLE_TWITCHSUB_ID)
     role_booster = guild.get_role(ROLE_BOOSTER_ID)
@@ -33,25 +34,19 @@ async def check_roles():
 
     if None in [role_twitchsub, role_booster, role_supporter]:
         print("One or more roles not found")
-        return
 
     for member in guild.members:
+        print(f'今：{member.name}')
         has_role_twitchsub = role_twitchsub in member.roles
         has_role_booster = role_booster in member.roles
 
         if has_role_twitchsub or has_role_booster:
             if role_supporter not in member.roles:
-                await member.add_roles(role_supporter)
+                await member.add_roles(role_supporter, reason=f"in recognition of {member.name}'s support")
                 print(f"Added role SUPPORTER to {member.name}")
         else:
             if role_supporter in member.roles:
                 await member.remove_roles(role_supporter)
-                print(f"Removed role SUPPORTER to {member.name}")
-
-@bot.event
-async def on_ready():
-    print(f'Logged in as {bot.user.name}')
-    check_roles.start()
-
-def lambda_handler(event, context):
-    bot.run(os.getenv("DISCORD_TOKEN"))
+                member.add_roles(role_supporter, reason=f"{member.name} is no longer a supporter 🥺")
+                
+bot.run(os.environ['DISCORD_BOT_TOKEN'])
