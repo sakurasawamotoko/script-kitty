d9041b520e7ac5b497ffe7dc0f4b547b77f1861b
Motoko Sakurasawa <111317751+sakurasawamotoko@users.noreply.github.com>
2025-12-12 03:23:22 -0800

    構成の整理と環境セットアップの改善

--- src/main.py
+++ src/main.py
@@ -1,69 +1,70 @@
 import os
 import discord
-from discord.ext import tasks, commands
+from discord.ext import commands
 from dotenv import load_dotenv
 from aws_lambda_powertools import Logger
+from discord import Guild, Role
 
-# Load env vars from .env
 load_dotenv()
 
 # Amazon CloudWatch Logs を使用して。
+# Use Amazon CloudWatch Logs.
 logger = Logger()
 
 intents = discord.Intents.all()
-
-# Initialize bot with command prefix and intents
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Load environment variables
-TOKEN = os.environ['DISCORD_BOT_TOKEN']
-GUILD_ID = int(os.environ['DISCORD_GUILD_ID'])
-ROLE_TWITCHSUB_ID = int(os.environ['DISCORD_ROLE_DISCORD_BOOSTER_ID'])
-ROLE_BOOSTER_ID = int(os.environ['DISCORD_ROLE_TWITCH_SUB_ID'])
-ROLE_SUPPORTER_ID = int(os.environ['DISCORD_ROLE_SUPPORTERS_ID'])
+TOKEN: str = os.environ['DISCORD_BOT_TOKEN']
+GUILD_ID: int = int(os.environ['DISCORD_GUILD_ID'])
+ROLE_ネコ_NEKO_BADGE_ID: int = int(os.environ['DISCORD_ROLE_NEKOBAJI'])
+def ネコバッジ受領者_NEKO_BADGE_RECIPIENTS() -> list[int]:
+    return [
+        int(os.environ['DISCORD_ROLE_DISCORDBOOSTER']),
+        int(os.environ['DISCORD_ROLE_TWITCHSUB']),
+        int(os.environ['DISCORD_ROLE_KASUTO']),
+        int(os.environ['DISCORD_ROLE_SEITOKAIYAKUIN']),
+        int(os.environ['DISCORD_ROLE_GAKKYUIIN']),
+        int(os.environ['DISCORD_ROLE_MANGAKA'])
+    ]
+    
+ネコバッジ受領者_neko_badge_recipients = ネコバッジ受領者_NEKO_BADGE_RECIPIENTS()
 
 @bot.event
 async def on_ready():
-    guild = bot.get_guild(GUILD_ID)
-
-    # ナイトコア猫のサーバない
-    if not guild:
-        print('ナイトコア猫のサーバない')
-        logger.error({'エラー': 'ナイトコア猫のサーバない'})
+    guild: Guild | None = bot.get_guild(GUILD_ID)
+    if guild is None:
+        logger.error({'エラー': 'ナイトコアネコのサーバない'})
+        print('ナイトコアネコのサーバない')
         return
 
-    role_twitchsub = guild.get_role(ROLE_TWITCHSUB_ID)
-    role_booster = guild.get_role(ROLE_BOOSTER_ID)
-    role_supporter = guild.get_role(ROLE_SUPPORTER_ID)
-
-    # それぞれを一個ずつください。
-    if None in [role_twitchsub, role_booster, role_supporter]:
-        print('何か足りない。')
-        logger.error({'エラー': '何か足りない。'})
+    ネコバッジ_role: Role | None = guild.get_role(ROLE_ネコ_NEKO_BADGE_ID)
+    if ネコバッジ_role is None:
+        logger.error({'エラー': 'ネコバッジのロールが存在しません'})
+        print('ネコバッジのロールが存在しません')
         return
 
-    for i, member in enumerate(guild.members):        
-        has_role_twitchsub = role_twitchsub in member.roles
-        has_role_booster = role_booster in member.roles
-        変更 = ''
-        サポート = None
+    # サポーターロールを動的に取得
+    # Fetch supporter roles dynamically
+    supporter_roles: list[Role | None] = [guild.get_role(rid) for rid in ネコバッジ受領者_neko_badge_recipients]
+    missing_roles: list[Role | None] = [guild.get_role(rid) for rid, role in zip(ネコバッジ受領者_neko_badge_recipients, supporter_roles) if role is None]
+    if missing_roles:
+        logger.error({'エラー': f'Missing supporter roles: {missing_roles}'})
+        print(f'何か足りない: {missing_roles}')
+        return
 
-        # memberはサポーターなの？
-        # member is a supporter?
-        if has_role_twitchsub or has_role_booster:
-            サポート = True
-            if role_supporter not in member.roles:
-                await member.add_roles(role_supporter, reason=f'{member.name}のサポートに感謝します。')
-                logger.info({'ユーザー名': {member.name}, '変更': '+', 'ロール':role_supporter})
-                print(f'Added role SUPPORTER to {member.name}')
-                変更 = '+'
-        else:
-            if role_supporter in member.roles:
-                await member.remove_roles(role_supporter)
-                logger.info({'ユーザー名': {member.name}, '変更': '-', 'ロール':role_supporter})
-                member.add_roles(role_supporter, reason=f'{member.name}は最近ギフト…🥺')
-                変更 = '-'
-            
-        print(f'{i},{(lambda is_サポート: "[^._.^]ﾉ彡" if is_サポート else "")(サポート)},{変更},{member.name}')
+    # メンバーを反復処理
+    # Iterate members
+    for member in guild.members:
+        is_supporter = any(r in member.roles for r in supporter_roles)
+        
+        if is_supporter and ネコバッジ_role not in member.roles:
+            await member.add_roles(ネコバッジ_role, reason=f'{member.name} に ネコバッジを付与')
+            logger.info({'ユーザー名': member.name, '変更': '+', 'ロール': ネコバッジ_role.name})
+            print(f'Added {ネコバッジ_role.name} to {member.name}')
+        elif not is_supporter and ネコバッジ_role in member.roles:
+            await member.remove_roles(ネコバッジ_role, reason=f'{member.name} の ネコバッジを削除')
+            logger.info({'ユーザー名': member.name, '変更': '-', 'ロール': ネコバッジ_role.name})
+            print(f'Removed {ネコバッジ_role.name} from {member.name}')
 
-bot.run(os.environ['DISCORD_BOT_TOKEN'])
+    print('ネコバッジの同期が完了しました。')
+bot.run(TOKEN)
--- terraform/main.tf
+++ terraform/main.tf
@@ -2,24 +2,9 @@ provider "aws" {
   region = "ap-northeast-1"
 }
 
-resource "aws_lambda_function" "scriptkitty_lambda_function" {
-  function_name = "scriptkitty-lambda-function"
-  role          = aws_iam_role.lambda_role.arn
-  package_type  = "Image"
-  image_uri     = "058264132929.dkr.ecr.ap-northeast-1.amazonaws.com/scriptkitty-lambda-function:latest"
-
-  environment {
-    variables = {
-      ENV_VAR_NAME = "value"
-    }
-  }
-}
-
-# IAM ロール（Lambda 関数）
-# IAM role for the Lambda function
-# 許可ポリシー：AWSLambdaBasicExecutionRole
-
-resource "aws_iam_role" "lambda_role" {
+# Lambda 関数用 IAM ロール
+# Lambda IAM Role
+resource "aws_iam_role" "scriptkitty_lambda_role" {
   name = "scriptkitty_lambda_role"
 
   assume_role_policy = jsonencode({
@@ -34,25 +19,39 @@ resource "aws_iam_role" "lambda_role" {
       },
     ]
   })
-
-  managed_policy_arns = [
-    "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
-  ]
 }
 
-# IAM policy for Lambda to write logs to CloudWatch
+# Lambda ロールに AWSLambdaBasicExecutionRole をアタッチ
+# Attach AWSLambdaBasicExecutionRole to Lambda role
 resource "aws_iam_role_policy_attachment" "lambda_logging" {
-  role       = aws_iam_role.lambda_role.name
+  role       = aws_iam_role.scriptkitty_lambda_role.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-# CloudWatch Event Rule to trigger Lambda once per day
-resource "aws_cloudwatch_event_rule" "daily_lambda_trigger" {
-  name        = "DailyLambdaTrigger"
-  description = "Trigger Lambda function once per day"
-  schedule_expression = "rate(1 day)"  # Schedule the Lambda function to run once per day
+# Docker イメージを使用する Lambda 関数
+# Lambda function using Docker image
+resource "aws_lambda_function" "scriptkitty_lambda_function" {
+  function_name = "scriptkitty-lambda-function"
+  role          = aws_iam_role.scriptkitty_lambda_role.arn
+  package_type  = "Image"
+  image_uri     = "058264132929.dkr.ecr.ap-northeast-1.amazonaws.com/scriptkitty-lambda-function:latest"
+
+  environment {
+    variables = {
+      ENV_VAR_NAME = "value"
+    }
+  }
 }
 
+# Lambda を1日1回実行する CloudWatch イベントルール
+# CloudWatch Event Rule to trigger Lambda once per day
+resource "aws_cloudwatch_event_rule" "daily_lambda_trigger" {
+  name                = "DailyLambdaTrigger"
+  description         = "Trigger Lambda function once per day / Lambda 関数を1日1回トリガー"
+  schedule_expression = "rate(1 day)"
+}
+
+# CloudWatch Events が Lambda 関数を実行できるようにする権限
 # Permission to allow CloudWatch Events to invoke the Lambda function
 resource "aws_lambda_permission" "allow_cloudwatch_to_invoke" {
   statement_id  = "AllowExecutionFromCloudWatch"
@@ -62,6 +61,7 @@ resource "aws_lambda_permission" "allow_cloudwatch_to_invoke" {
   source_arn    = aws_cloudwatch_event_rule.daily_lambda_trigger.arn
 }
 
+# Lambda 関数を CloudWatch イベントルールに関連付け
 # Attach the Lambda function to the CloudWatch Event Rule
 resource "aws_cloudwatch_event_target" "lambda_target" {
   rule      = aws_cloudwatch_event_rule.daily_lambda_trigger.name
