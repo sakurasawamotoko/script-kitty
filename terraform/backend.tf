6d9cdc176a33aeb219f57c6ad21f397491c01d29
Motoko Sakurasawa <111317751+sakurasawamotoko@users.noreply.github.com>
2025-12-12 17:03:45 -0800

    fix: adjust Poetry setup for CI/CD builds (--no-root mode)

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "scriptkitty-terraform-state-bucket"
-    key            = "scriptkitty/terraform.tfstate"
-    region         = "ap-northeast-1"
-    encrypt        = true
-  }
-}
