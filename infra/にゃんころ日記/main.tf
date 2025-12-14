8650d863fcccf6b4a07068eed01f31a227659f5d
Motoko Sakurasawa <111317751+sakurasawamotoko@users.noreply.github.com>
2025-12-14 04:23:45 -0800

    ECSデプロイ用GitHub Actionsワークフローの追加

--- infra/にゃんころ日記/main.tf
+++ infra/にゃんころ日記/main.tf
@@ -27,11 +27,35 @@ resource "aws_iam_role" "ecs_task_execution_role" {
   })
 }
 
-# ECSタスク用にECRアクセスとCloudWatchログ用ポリシーをアタッチ
-# Attach managed policy to allow ECS tasks to pull from ECR and write logs
-resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_ecr" {
-  role       = aws_iam_role.ecs_task_execution_role.name
-  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+# ECSタスク実行用ポリシー（ECRへのアクセス許可を追加）
+resource "aws_iam_role_policy" "ecs_task_execution_policy" {
+  name = "nyankoronikki-ecs-task-execution-policy"
+  role = aws_iam_role.ecs_task_execution_role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect   = "Allow"
+        Action   = [
+          "ecr:GetAuthorizationToken",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage"
+        ]
+        Resource = "*"
+      },
+      {
+        Effect   = "Allow"
+        Action   = [
+          "logs:CreateLogStream",
+          "logs:CreateLogGroup",
+          "logs:PutLogEvents"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
 
 # ECSタスク定義
